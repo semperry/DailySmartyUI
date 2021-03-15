a376ec89082ccfee326c4c55235fdb335e6043bf
semperry <ryan.curtis88@gmail.com>
2021-03-15 13:36:52 -0600

    sets up logo component and corresponding styles

---
+++ src/components/logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default () => {
+	return (
+		<div className="logo-main">
+			<img
+				src="/assets/ds_circle_logo.png"
+				alt="daily smarty ui image logo big"
+			/>
+		</div>
+	);
+};
